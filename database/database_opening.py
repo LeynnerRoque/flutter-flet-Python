19b45b15364b130f77bb433d7e843d4912749d06
leynnerroque <leynnerroque@gmail.com>
2022-04-09 23:22:58 -0300

    public branch

--- database/database_opening.py
+++ database/database_opening.py
@@ -7,8 +7,8 @@ conection = pymysql.connect(
     host='localhost',
     port=3306,
     user='root',
-    password='*****',
-    database='database',
+    password='roque1989',
+    database='appjobs',
     autocommit=True
 )
 
